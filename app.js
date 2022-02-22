562d61fa791bac6ef75d6ebf3d00e8a36b0ba6de
Rodrigo Gutierrez <rodrigo644@gmail.com>
2022-02-22 12:35:11 -0300

    Refactor app.js database and models

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
 const express = require('express');
-const Sequelize = require('sequelize');
+const Playlist = require('./models/playlist');
+//const Sequelize = require('sequelize');
 
 const app = express();
 
-const sequelize = new Sequelize('sqlite:chinook.db');
-
-const Playlist = sequelize.define('playlist', {
-    id: {
-        field: 'PlaylistId',
-        type: Sequelize.INTEGER,
-        primaryKey: true
-    },
-    name: {
-        field: 'Name',
-        type: Sequelize.STRING
-    }
-}, {
-    timestamps: false
-});
 
 app.get('/api/playlists', (req, res) => {
     Playlist.findAll().then((playlists) => {
